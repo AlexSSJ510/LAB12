a073afaa47805dc2f80a0f9e7c5d4795d0fd11ad
Alex <75244564@continental.edu.pe>
2025-11-06 08:39:59 -0500

    Rifle
    Riffle falta

--- Assets/Scripts/Player/Rifle.cs
+++ Assets/Scripts/Player/Rifle.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Rifle : MonoBehaviour
 {
     public float range = 50f;
-    public KeyCode fireKey = KeyCode.F;
+    public Key fireKey = Key.F;  // Use 'Key' instead of 'KeyCode' for the new Input System
+
+    private PlayerInputActions _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new PlayerInputActions(); // Make sure you have this class set up for input
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Player.Enable();
+        _inputActions.Player.Fire.performed += OnFire;  // Listen for the 'Fire' action
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Fire.performed -= OnFire;
+        _inputActions.Player.Disable();
+    }
+
+    private void OnFire(InputAction.CallbackContext context)
+    {
+        Shoot();
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(fireKey))
-        {
-            Shoot();
-        }
+   
     }
 
     private void Shoot()
